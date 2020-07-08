613bcdee500d1c3fdd8fa7b5d46eefd35fa5b895
Brian McPhillips <B.McPhillips89@gmail.com>
2020-07-08 15:34:51 -0700

    html and css roughly set up, starting JavaScript.

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 // import functions and grab DOM elements
+const userInput = document.querySelector('input:checked');
+const current = document.querySelector('#current');
+const wins = document.querySelector('#wins');
+const losses = document.querySelector('#losses');
+const total = document.querySelector('#total');
 
 // initialize state
+let winsData = 0;
+
+let totalTries = 0;
+
+let drawAmount = 0;
+
+let resetAmount = 0;
+
+let randomThrow = 0;
+
 
 // set event listeners to update state and DOM
