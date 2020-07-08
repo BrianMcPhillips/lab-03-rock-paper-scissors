1745f44f98f5d0b5e36130df6277c0cc91c3bd08
Brian McPhillips <B.McPhillips89@gmail.com>
2020-07-08 16:01:41 -0700

    user input working, and total tries counter working.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // import functions and grab DOM elements
-const userInput = document.querySelector('input:checked');
+
+const submitButton = document.querySelector('#play-button');
 const current = document.querySelector('#current');
 const wins = document.querySelector('#wins');
 const losses = document.querySelector('#losses');
@@ -18,3 +19,14 @@ let randomThrow = 0;
 
 
 // set event listeners to update state and DOM
+submitButton.addEventListener('click', () => { 
+    totalTries++;
+    const userInput = document.querySelector('input:checked');
+    const userGuess = userInput.value;
+    
+//created user input and total number if "tries"
+    const randomNumber = Math.ceil(Math.random() * 3);
+//generated random number from 1 to 3 
+
+    
+});
