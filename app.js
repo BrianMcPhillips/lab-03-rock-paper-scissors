942219f51e41ef7c5feacaffbb3ac1e28189b7db
Brian McPhillips <B.McPhillips89@gmail.com>
2020-07-08 21:25:27 -0700

    JavaScript fully functional, need to add HTML content and style CSS.

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const resetButton = document.querySelector('#reset-button');
 const currentState = document.querySelector('#current');
 const wins = document.querySelector('#wins');
 const losses = document.querySelector('#losses');
+const draw = document.querySelector('#draw');
 const total = document.querySelector('#total');
+const reset = document.querySelector('#reset');
 
 // initialize state
 let winsData = 0;
@@ -40,5 +42,34 @@ submitButton.addEventListener('click', () => {
 
     if (gamePlay === 'draw') {currentState.textContent = 'Draw'; drawData++;} 
 
+    wins.textContent = 'Wins: ' + winsData;
+    losses.textContent = 'Losses ' + loseData;
+    draw.textContent = 'Draws ' + drawData;
+    total.textContent = 'Total plays ' + totalTries;
 });
 
+//function resetTheGame() {
+    //const doReset = window.confirm('Are you sure you want to lose your score?');
+
+    //if (doReset === true) {
+        
+resetButton.addEventListener('click', () => {
+    
+    resetAmount++;
+    totalTries = 0;
+    winsData = 0;
+    loseData = 0;
+    drawData = 0;
+
+    wins.textContent = 'Wins: 0'; 
+    losses.textContent = 'Losses ' + loseData;
+    draw.textContent = 'Draws ' + drawData;
+    total.textContent = 'Total plays ' + totalTries;
+    reset.textContent = resetAmount;
+    //}
+
+    
+ 
+
+    
+});
